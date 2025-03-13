f29644392b6249a353d2a15867dcb43f457b2319
Jetross <jetrossneri07@gmail.com>
2025-03-13 23:13:24 +0800

    done setting up code for AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
     public function register(): void
     {
@@ -16,9 +19,12 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
     public function boot(): void
     {
-        //
+        // Set default string length for database migrations
+        Schema::defaultStringLength(191);
     }
 }
