f4a79eb9ffeb9567b97e4ee789c67c4009973607
Jetross <jetrossneri07@gmail.com>
2025-03-15 11:59:15 +0800

    done setting up code for DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Uncomment the line below to create 10 random users
         // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Add additional seeders here
+        // $this->call([
+        //     AnotherSeeder::class,
+        // ]);
     }
 }
