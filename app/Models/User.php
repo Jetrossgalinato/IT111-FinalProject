a3479ccde25ee5f61f67fb8087a209cf220bc3c0
Jetross <jetrossneri07@gmail.com>
2025-03-17 12:56:25 +0800

    added some files and modified User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,22 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Create a new personal access token for the user.
+     *
+     * @param  string  $name
+     * @param  array  $abilities
+     * @return \Laravel\Sanctum\NewAccessToken
+     */
+    public function createToken(string $name, array $abilities = ['*'])
+    {
+        $token = $this->tokens()->create([
+            'name' => $name,
+            'token' => hash('sha256', $plainTextToken = \Illuminate\Support\Str::random(40)),
+            'abilities' => $abilities,
+        ]);
+
+        return new \Laravel\Sanctum\NewAccessToken($token, $plainTextToken);
+    }
 }
