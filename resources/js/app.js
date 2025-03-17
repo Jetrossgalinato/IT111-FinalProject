9d602a95c3f1f53a54be2c8a55fee4469460d05b
Jetross <jetrossneri07@gmail.com>
2025-03-17 12:18:24 +0800

    done setting up code for app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "./components/App.vue";
+
+// Create a new Vue application instance
+const app = createApp(App);
+
+// Mount the Vue application to an HTML element with the ID 'app'
+app.mount("#app");
